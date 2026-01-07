cfa718cc96e16a4fd848790a91ff75aedd753db7
oThiagoSouza <souzaalvesthiago99@gmail.com>
2026-01-07 05:21:04 -0300

    layout

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+export default function Index() {
+    return (
+        <View style={{ flex: 1, marginTop: 50,
+            alignItems:"center"
+        }}>
+            <Text style ={{ fontSize: 36, color: '#fff'}}>Hello React Native</Text>
+        </View>
+            
+
+    )
+}
